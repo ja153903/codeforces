5317191e3263849c9b6736afa77d229188b09901
jaime <jaimeabbariao@gmail.com>
2020-03-28 12:46:26 -0400

    Updated solution. Should think more clearly.

--- 959A_mahmoud_and_ehab.cc
+++ 959A_mahmoud_and_ehab.cc
@@ -9,56 +9,14 @@ int main()
     int n;
     cin >> n;
 
-    // Given M always starts
-    // we can just give him the win if the number generated is even
     if (n % 2 == 0)
     {
         cout << "Mahmoud" << endl;
-        return 0;
     }
-
-    bool m_win = false;
-
-    for (int t = 1; n > 0; t++)
+    else
     {
-        if (t % 2 == 1)
-        {
-            // mahmoud's turn
-            if (n % 2 == 0)
-            {
-                n = 0;
-                m_win = true;
-            }
-            else
-            {
-                // if the value is odd and the next best even value is 0
-                // then we've lost
-                if (n - 1 == 0)
-                {
-                    m_win = false;
-                }
-                else
-                {
-                    n -= (n - 1);
-                }
-            }
-        }
-        else
-        {
-            if (n % 2 == 1)
-            {
-                n = 0;
-                m_win = false;
-            }
-            else
-            {
-                n -= (n - 1);
-            }
-        }
+        cout << "Ehab" << endl;
     }
-
-    m_win ? cout << "Mahmoud" : cout << "Ehab";
-    cout << endl;
 
     return 0;
 }
